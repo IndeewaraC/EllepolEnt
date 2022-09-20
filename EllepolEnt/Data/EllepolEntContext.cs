03d095b8e43fd86c341c363e3df302dd32c826cc
indeewara gunathilaka <indeewaragunathilaka@gmail.com>
2022-09-21 00:17:19 +0530

    Add project files.

---
+++ EllepolEnt/Data/EllepolEntContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EllepolEnt.Models;
+
+namespace EllepolEnt.Data
+{
+    public class EllepolEntContext : DbContext
+    {
+        public EllepolEntContext (DbContextOptions<EllepolEntContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<EllepolEnt.Models.tbl_Login> tbl_Login { get; set; } = default!;
+    }
+}
